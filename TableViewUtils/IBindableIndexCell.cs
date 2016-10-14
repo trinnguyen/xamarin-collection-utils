c858092b0f696c045db6509b564b3a04aa6a3462
Tri Nguyen <tringuyen.nnt@gmail.com>
2016-10-14 15:14:24 +0700

    Init source code

---
+++ TableViewUtils/IBindableIndexCell.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using UIKit;
+
+namespace TableViewUtils
+{
+	public interface IBindableIndexCell<T>
+	{
+		void Bind(T cellViewModel, UITableView tableView, Foundation.NSIndexPath indexPath);
+	}
+}
